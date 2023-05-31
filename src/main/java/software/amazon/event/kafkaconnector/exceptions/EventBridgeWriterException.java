239e939d2ef03a947078628bddcb8f8deaf5a099
Michael Gasch <15986659+embano1@users.noreply.github.com>
2023-05-31 11:35:21 +0200

    ci: Enforce licenses
    Closes: #21
    Signed-off-by: Michael Gasch <15986659+embano1@users.noreply.github.com>

--- src/main/java/software/amazon/event/kafkaconnector/exceptions/EventBridgeWriterException.java
+++ src/main/java/software/amazon/event/kafkaconnector/exceptions/EventBridgeWriterException.java
@@ -1,8 +1,7 @@
 /*
- * // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * // SPDX-License-Identifier: Apache-2.0
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
  */
-
 package software.amazon.event.kafkaconnector.exceptions;
 
 public class EventBridgeWriterException extends RuntimeException {
