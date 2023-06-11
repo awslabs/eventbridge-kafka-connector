2e11b6cad33407b519ae4054eb116ac5643dd062
Michael Gasch <15986659+embano1@users.noreply.github.com>
2023-06-11 08:26:55 +0200

    ci: allow longer commitlint bodies
    commitlint errors when body is larger than 200 chars, instead of 100 (default config)
    
    Closes: #44
    Signed-off-by: Michael Gasch <15986659+embano1@users.noreply.github.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,9 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     // Any rules defined here will override rules from @commitlint/config-conventional
     rules: {
+        // warn only
         'subject-case': [1, 'always', 'lower-case'],
+        // fail
+        'body-max-length': [2, 'always', 200]
     },
 }
